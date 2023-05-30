6f896300cceaa6ddb747b0b0943bab1f05b375f5
zoheirkabuli <zoheyrk@gmail.com>
2023-05-30 19:44:49 +0330

    complete hero sliders

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+const heroSlider = new Swiper(".hero-slider", {
+  slidesPerView: 1,
+  rewind: true,
+});
 
+const heroThumbSlider = new Swiper(".hero-thumb-slider", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  rewind: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+heroSlider.controller.control = heroThumbSlider;
+heroThumbSlider.controller.control = heroSlider;
